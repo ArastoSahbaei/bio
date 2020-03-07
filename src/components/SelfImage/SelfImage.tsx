56bc735bf3958063177d56f290f596b270858e32
arasto <Arasto.Sahbaei@gmail.com>
2020-03-07 07:09:32 +0100

    hover effect update

--- src/components/SelfImage/SelfImage.tsx
+++ src/components/SelfImage/SelfImage.tsx
@@ -5,7 +5,7 @@ import './SelfImage.css'
 export default function SelfImage() {
     return (
         <div>
-            <img className="arastoImage" src={arastoImage} alt="arre" />
+            <img className="arastoImage" src={arastoImage} alt="Loading.." />
         </div>
     )
 }
