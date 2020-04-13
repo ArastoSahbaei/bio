ec7f9c6c40adbae6cd410436d9aa263381a2e714
arasto <Arasto.Sahbaei@gmail.com>
2020-04-13 11:13:11 +0200

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import './utils/images/AxeHandel.ttf'
 import './utils/design/Global.css'
 import { Home } from './view/home/Home'
+import { Contact } from './view/contact/Contact'
 import SocialMedia from './components/socialmedia/SocialMedia'
 
 export default function App() {
   return (
     <div className="App">
       <Home />
+      <Contact />
       <SocialMedia />
     </div>
   );
---
+++ src/view/contact/Contact.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contact.css'
+
+export const Contact = () => {
+    return (
+        <div className="contactContainer">
+            <h1 className="contactSection">This is the contact section</h1>
+        </div>
+    )
+}
