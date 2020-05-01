ce7335cb72b349165401c09a2bbc2e819948cf73
arasto <Arasto.Sahbaei@gmail.com>
2020-05-01 08:18:01 +0200

    m

--- src/view/about/About.tsx
+++ src/view/about/About.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import DeveloperImg from '../../utils/images/developer.svg'
+import LocationImg from '../../utils/images/location.svg'
+import HeartImg from '../../utils/images/heart.svg'
+import Quote from '../../utils/images/quote.svg'
 import './About.css'
 
 export const About = () => {
     return (
         <div className="aboutContainer">
             <p className="aboutMeTitle">
-                <img className="developerImage" src={DeveloperImg} alt="LOL" />
-                I'm a web designer / developer in Gothenburg, Sweden. Building modern, state-of-the-art,
-                user-friendly websites and applications is truly a passion of mine.</p>
+                Arasto Sahbaei, 27. 
+                <img className="developerImage" src={DeveloperImg} alt="" />
+                <br />
+                <img className="developerImage" src={Quote} alt="" />
+                " Building modern, state-of-the-art, user-friendly websites and applications is truly a passion of mine. "</p>
+
             <p className="aboutMeTitle2">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi perferendis impedit voluptate tempore unde molestiae odit, hic minus repellat non.
-                <img className="developerImage" src={DeveloperImg} alt="LOL" />
+                Currently living in Gothenburg, Sweden
+                <img className="developerImage" src={LocationImg} alt="" />
+            </p>
+
+            <p className="aboutMeTitle3">
+                <img className="developerImage" src={HeartImg} alt="" />
+                Family, friends, my dog, outdoor life, chess & pizza!
             </p>
         </div>
     )
