f701089caa135b129a56d2257c12c09c5aa4acee
arasto <Arasto.Sahbaei@gmail.com>
2020-05-01 13:49:19 +0200

    added new section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './utils/images/AxeHandel.ttf'
 import './utils/design/Global.css'
 import { Home } from './view/home/Home'
 import { About } from './view/about/About'
+import { Work } from './view/work/Work'
 import { Contact } from './view/contact/Contact'
 import SocialMedia from './components/socialmedia/SocialMedia'
 
@@ -12,6 +13,7 @@ export default function App() {
     <div className="App">
       <Home />
       <About />
+      <Work />
       <Contact />
       <SocialMedia />
     </div>
---
+++ src/view/work/Work.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Work.css'
+
+export const Work = () => {
+    return (
+        <div className="workContainer">
+            <h1>Previous work, recommendations & skills</h1>
+        </div>
+    )
+}
