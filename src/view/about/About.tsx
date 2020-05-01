8b8d14de09f467fc66ef71f22a0b3fc62f35ec8e
arasto <Arasto.Sahbaei@gmail.com>
2020-05-01 09:07:46 +0200

    update

--- src/view/about/About.tsx
+++ src/view/about/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DeveloperImg from '../../utils/images/developer.svg'
+import DeveloperImg from '../../utils/images/avatar.svg'
 import LocationImg from '../../utils/images/location.svg'
 import HeartImg from '../../utils/images/heart.svg'
 import Quote from '../../utils/images/quote.svg'
@@ -8,22 +8,24 @@ import './About.css'
 export const About = () => {
     return (
         <div className="aboutContainer">
-            <p className="aboutMeTitle">
-                Arasto Sahbaei, 27. 
-                <img className="developerImage" src={DeveloperImg} alt="" />
-                <br />
+            <p className="aboutMeTitle"> Arasto Sahbaei, 27. <br /><br /><br /> lé pitch
+            <img className="developerImage" src={DeveloperImg} alt="" />
+            </p>
+
+            <div className="quoteContainer">
                 <img className="developerImage" src={Quote} alt="" />
-                " Building modern, state-of-the-art, user-friendly websites and applications is truly a passion of mine. "</p>
+                <p className="aboutMeTitle4">" Building modern, state-of-the-art, user-friendly websites and applications is truly a passion of mine. "</p>
+            </div>
 
-            <p className="aboutMeTitle2">
-                Currently living in Gothenburg, Sweden
+            <div className="locationContainer">
                 <img className="developerImage" src={LocationImg} alt="" />
-            </p>
+                <p className="aboutMeTitle2"> Currently living in Gothenburg, Sweden</p>
+            </div>
 
-            <p className="aboutMeTitle3">
+            <div className="heartContainer">
+                <p className="aboutMeTitle3">Family, friends, my dog, outdoor life, chess & pizza!</p>
                 <img className="developerImage" src={HeartImg} alt="" />
-                Family, friends, my dog, outdoor life, chess & pizza!
-            </p>
+            </div>
         </div>
     )
 }
