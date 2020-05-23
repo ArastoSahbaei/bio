352fefb523ca04216b793b2124a09f7f3a6ea924
arasto <Arasto.Sahbaei@gmail.com>
2020-05-23 09:54:38 +0200

    added card component

--- src/view/work/Work.tsx
+++ src/view/work/Work.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Work.css'
+import Card from "../../components/card/Card"
+import cardImage from "../../utils/images/dog.png"
 
 export const Work = () => {
     return (
         <div className="workContainer">
-            <h1>Previous work, recommendations & skills</h1>
+            <h1 className="workTitle">Previous work, recommendations & skills</h1>
+            <div className="cardName">
+                <Card
+                    image={cardImage}
+                    title="University of Gothenburg"
+                    date="2016 - 2017" />
+            </div>
         </div>
     )
 }
