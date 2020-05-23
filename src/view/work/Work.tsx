a75012e6c6058c822f8aeb3087573e9bb8b5bb6a
arasto <Arasto.Sahbaei@gmail.com>
2020-05-23 18:47:23 +0200

    added logotypes

--- src/view/work/Work.tsx
+++ src/view/work/Work.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Work.css'
 import Card from "../../components/card/Card"
 import cardImage from "../../utils/images/dog.png"
+import Gothenburg from "../../utils/images/gothenburg.png"
+import ITHS from "../../utils/images/ithogskolan.png"
+import RSM from "../../utils/images/RSM.png"
+import VolvoPenta from "../../utils/images/volvoPenta.png"
 
 export const Work = () => {
     return (
@@ -9,33 +13,33 @@ export const Work = () => {
             <h1 className="workTitle">Previous work, recommendations & skills</h1>
             <div className="cardName">
                 <Card
-                    image={cardImage}
+                    image={Gothenburg}
                     title="University of Gothenburg"
-                    date="2016 - 2017"
+                    date="2020 - "
                     description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
                     heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
             </div>
             <div className="cardName2">
                 <Card
-                    image={cardImage}
-                    title="University of Gothenburg"
-                    date="2016 - 2017"
+                    image={ITHS}
+                    title="IT-Högskolan"
+                    date="2019 - 2020"
                     description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
                     heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
             </div>
             <div className="cardName3">
                 <Card
-                    image={cardImage}
-                    title="University of Gothenburg"
-                    date="2016 - 2017"
+                    image={RSM}
+                    title="Ramboll"
+                    date="2019 - 2019"
                     description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
                     heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
             </div>
             <div className="cardName4">
                 <Card
-                    image={cardImage}
-                    title="University of Gothenburg"
-                    date="2016 - 2017"
+                    image={VolvoPenta}
+                    title="Volvo Penta"
+                    date="2018 - 2019"
                     description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
                     heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
             </div>
