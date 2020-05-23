901dc01671d98c9a9cbf45c37b0dea5a087c5edf
arasto <Arasto.Sahbaei@gmail.com>
2020-05-23 19:04:16 +0200

    added alten

--- src/view/work/Work.tsx
+++ src/view/work/Work.tsx
@@ -6,12 +6,21 @@ import Gothenburg from "../../utils/images/gothenburg.png"
 import ITHS from "../../utils/images/ithogskolan.png"
 import RSM from "../../utils/images/RSM.png"
 import VolvoPenta from "../../utils/images/volvoPenta.png"
+import Alten from "../../utils/images/Alten.png"
 
 export const Work = () => {
     return (
         <div className="workContainer">
             <h1 className="workTitle">Previous work, recommendations & skills</h1>
             <div className="cardName">
+                <Card
+                    image={Alten}
+                    title="Alten"
+                    date="2017 - "
+                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+            </div>
+            <div className="cardName1">
                 <Card
                     image={Gothenburg}
                     title="University of Gothenburg"
