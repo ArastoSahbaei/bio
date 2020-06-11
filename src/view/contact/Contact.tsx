aee3fce21d17b5a80827e45841a85dd503a90194
arasto <Arasto.Sahbaei@gmail.com>
2020-06-11 05:52:14 +0200

    form added

--- src/view/contact/Contact.tsx
+++ src/view/contact/Contact.tsx
@@ -5,6 +5,12 @@ export const Contact = () => {
     return (
         <div className="contactContainer">
             <h1 className="contactSection">This is the contact section</h1>
+            <form className="formContainer">
+                <input name="name" type="text" className="feedback-input" placeholder="Name" />
+                <input name="email" type="text" className="feedback-input" placeholder="Email" />
+                <textarea name="text" className="textArea-input" placeholder="Comment"></textarea>
+                <input type="submit" value="SUBMIT" />
+            </form>
         </div>
     )
 }
