c751e5165a91f27545b85f57a27ad609c44399c2
arasto <Arasto.Sahbaei@gmail.com>
2020-06-11 05:13:08 +0200

    text update

--- src/view/work/Work.tsx
+++ src/view/work/Work.tsx
@@ -16,40 +16,36 @@ export const Work = () => {
                     image={Alten}
                     title="Alten"
                     date="2017 - "
-                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+                    description="After doing my internship at Alten during my studies I was offered an employment which is still ongoing." />
             </div>
             <div className="cardName1">
                 <Card
                     image={Gothenburg}
                     title="University of Gothenburg"
                     date="2020 - "
-                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+                    description="Major system integration with the purpose to effortless maintain user data within the corporation. Together with my tech-lead we shared the responsibility for the development of the client including UI/UX." />
             </div>
             <div className="cardName2">
                 <Card
                     image={ITHS}
                     title="IT-Högskolan"
                     date="2019 - 2020"
-                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+                    description="Reunited with the teachers that once taught me, I was the course coordinator & lecturer for the class of 2019 during the first semester." />
             </div>
             <div className="cardName3">
                 <Card
                     image={RSM}
                     title="Ramboll"
                     date="2019 - 2019"
-                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+                    description="Given a finished UI design I was assigned to implement it to update their old client which was previously built with the JSF framework." />
             </div>
             <div className="cardName4">
                 <Card
                     image={VolvoPenta}
                     title="Volvo Penta"
                     date="2018 - 2019"
-                    description="das Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightlylorem description" />
+                    description="Proof-of-concept project to provide the customer a security system with purpose of preventing counterfeit products. 
+                    I participated throughout the lifespan of this project and had great success in leading the project from the very first meeting until we had a finished implementation." />
             </div>
         </div>
     )
