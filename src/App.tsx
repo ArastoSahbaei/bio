7900e2a4af636643a205069872564ec5efe1d055
arasto <Arasto.Sahbaei@gmail.com>
2020-07-09 16:00:31 +0200

    updated font and imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import './utils/images/AxeHandel.ttf'
 import './utils/design/Global.css'
+import './utils/fonts/FredokaOne-Regular.ttf';
 import { Home } from './view/home/Home'
 import { About } from './view/about/About'
 import { Work } from './view/work/Work'
 import { Contact } from './view/contact/Contact'
 import SocialMedia from './components/socialmedia/SocialMedia'
+
 
 export default function App() {
   return (
